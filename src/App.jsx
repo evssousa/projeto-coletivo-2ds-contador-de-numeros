52f69a76f6f39671781ae0f553a5f6a3ba2b40ee
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2024-10-28 00:34:19 -0300

    Revert "Projeto Finalizado!"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import Counter from "./components/Counter"
-
 export default function App() {
   return (
-    <div className="container">
+    <div>
       <h1>Contador de Números</h1>
-      <Counter/>
     </div>
   )
 }

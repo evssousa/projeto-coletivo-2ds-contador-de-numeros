d3695036536096918808ffd18ebc1e5bd7624d63
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2024-11-06 14:05:26 +0000

    corrigido problema de fechamento de div

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,10 @@
+// última correção: problema de fechamento de div pela Evellyn.
+// corrigido! Próxima pessoa do requisito que mexer aqui pode apagar esse comentário...
 export default function Button(props) {
-    return(
-    <div>
-        <button>{props.nameIncrement}</button>
-        <button>{props.nameDecrement}</button>
-    <div/>
-})
+    return (
+        <div>
+            <button>{props.nameIncrement}</button>
+            <button>{props.nameDecrement}</button>
+        </div>
+    )
+}

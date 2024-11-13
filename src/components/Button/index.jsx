bef2ecb1ba79e025e2e77150a0448456778f8e91
Everson Sousa <67715053+evssousa@users.noreply.github.com>
2024-11-13 16:32:28 -0300

    Delete src directory

--- src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button(props) {
-    return (
-        <div>
-            <button>{props.nameIncrement}</button>
-            <button>{props.nameDecrement}</button>
-            <button>{props.nameReset}</button>
-        </div>
-    )
-}
--- src/components/Title/index.jsx
+++
@@ -1,6 +0,0 @@
-export default function Title(props) {
-  return (
-     <div>
-       <h1>{props.children}</h1>
-     </div>
-  )}
